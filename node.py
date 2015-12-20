0a7f209728d916842e1c68dfa3494f5988ae150a
Caipers <spcaipers@gmail.com>
2015-12-20 16:38:30 -0200

    Adicionado validacao dos atributos na classe node.

--- node.py
+++ node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 
+import re
+
 class node():
     """Class that implements a Zigbee network node"""
 
@@ -12,16 +14,37 @@ class node():
             panAdr = 16-bits Wireless PAN Network
             neighbors = List of Dictionaries including nwkAdr, incoming and outcoming costs of the node's neighbor
         """
+        
+        self.r_nwkAdr = re.compile("0x[0-9a-f][0-9a-f][0-9a-f][0-9a-f]", re.IGNORECASE)
+        self.r_panAdr = re.compile("0x[0-9a-f][0-9a-f][0-9a-f][0-9a-f]", re.IGNORECASE)
+        self.r_macAdr = re.compile("[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]", re.IGNORECASE)
+        
+        if (self.r_nwkAdr.match(nwkAdr) == None):
+            raise AttributeError('Incorrect nwkAdr')
+        if (self.r_panAdr.match(panAdr) == None):
+            raise AttributeError('Incorrect panAdr')
+        if (self.r_macAdr.match(macAdr) == None):
+            raise AttributeError('Incorrect macAdr')
+        
         self.nwkAdr = nwkAdr
         self.macAdr = macAdr
         self.panAdr = panAdr
         self.neighbors = []
 
     def setnwkAdr(self, nwkAdr):
+        if (self.r_nwkAdr.match(nwkAdr) == None):
+            raise AttributeError('Incorrect nwkAdr')
+
         self.nwkAdr = nwkAdr
     def setMacAdr(self, macAdr):
+        if (self.r_macAdr.match(macAdr) == None):
+            raise AttributeError('Incorrect macAdr')
+
         self.macAdr = macAdr
     def setPanAdr(self, panAdr):
+        if (self.r_panAdr.match(panAdr) == None):
+            raise AttributeError('Incorrect panAdr')
+
         self.panAdr = panAdr
     def setNeighbors(self, neighbors):
         self.neighbors = neighbors
